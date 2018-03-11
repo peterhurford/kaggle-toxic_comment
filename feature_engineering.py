e2ef40bab9da3356db898f4b5c22ceaf0ea43620
Peter Hurford <peter.hurford@peter.hurford.MacBook>
2018-03-11 18:37:27 -0500

    Fix typos in FE; add repeating sentence, IP Address, and new words to FE

--- feature_engineering.py
+++ feature_engineering.py
@@ -34,121 +34,129 @@ def add_features(train, test):
     test2 = test.copy()
 
     # Basic features
-    print_step('BASIC FE 1/28')
+    print_step('BASIC FE 1/30')
     train2['num_words'] = train2['comment_text'].apply(lambda x: len(str(x).split()))
     test2['num_words'] = test2['comment_text'].apply(lambda x: len(str(x).split()))
 
-    print_step('BASIC FE 2/28')
+    print_step('BASIC FE 2/30')
     train2['num_unique_words'] = train2['comment_text'].apply(lambda x: len(set(str(x).lower().split())))
     test2['num_unique_words'] = test2['comment_text'].apply(lambda x: len(set(str(x).lower().split())))
 
-    print_step('BASIC FE 3/28')
+    print_step('BASIC FE 3/30')
     train2['unique_words_per_word'] = train2['num_unique_words'] / (train2['num_words'] + 0.0001)
     test2['unique_words_per_word'] = test2['num_unique_words'] / (test2['num_words'] + 0.0001)
 
-    print_step('BASIC FE 4/28')
+    print_step('BASIC FE 4/30')
     train2['num_chars'] = train2['comment_text'].apply(lambda x: len(str(x)))
     test2['num_chars'] = test2['comment_text'].apply(lambda x: len(str(x)))
 
-    print_step('BASIC FE 5/28')
+    print_step('BASIC FE 5/30')
     train2['num_capital'] = train2['comment_text'].apply(lambda x: len([c for c in x if c.isupper()]))
     test2['num_capital'] = test2['comment_text'].apply(lambda x: len([c for c in x if c.isupper()]))
 
-    print_step('BASIC FE 6/28')
+    print_step('BASIC FE 6/30')
     train2['num_lowercase'] = train2['comment_text'].apply(lambda x: len([c for c in x if c.islower()]))
     test2['num_lowercase'] = test2['comment_text'].apply(lambda x: len([c for c in x if c.islower()]))
 
-    print_step('BASIC FE 7/28')
+    print_step('BASIC FE 7/30')
     train2['capital_per_char'] = train2['num_capital'] / train2['num_chars']
     test2['capital_per_char'] = test2['num_capital'] / test2['num_chars']
 
-    print_step('BASIC FE 8/28')
+    print_step('BASIC FE 8/30')
     train2['lowercase_per_char'] = train2['num_lowercase'] / train2['num_chars']
     test2['lowercase_per_char'] = test2['num_lowercase'] / test2['num_chars']
 
-    print_step('BASIC FE 9/28')
+    print_step('BASIC FE 9/30')
     stop_words = {x: 1 for x in stopwords.words('english')}
     train2['num_stopwords'] = train2['comment_text'].apply(lambda x: len([w for w in str(x).lower().split() if w in stop_words]))
     test2['num_stopwords'] = test2['comment_text'].apply(lambda x: len([w for w in str(x).lower().split() if w in stop_words]))
 
-    print_step('BASIC FE 10/28')
+    print_step('BASIC FE 10/30')
     train2['num_punctuations'] = train2['comment_text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
     test2['num_punctuations'] = test2['comment_text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
 
-    print_step('BASIC FE 11/28')
+    print_step('BASIC FE 11/30')
     train2['punctuation_per_char'] = train2['num_punctuations'] / train2['num_chars']
     test2['punctuation_per_char'] = test2['num_punctuations'] / test2['num_chars']
 
-    print_step('BASIC FE 12/28')
+    print_step('BASIC FE 12/30')
     train2['num_words_upper'] = train2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
     test2['num_words_upper'] = test2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
 
-    print_step('BASIC FE 13/28')
+    print_step('BASIC FE 13/30')
     train2['num_words_lower'] = train2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.islower()]))
     test2['num_words_lower'] = test2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.islower()]))
 
-    print_step('BASIC FE 14/28')
+    print_step('BASIC FE 14/30')
     train2['num_words_title'] = train2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
     test2['num_words_title'] = test2['comment_text'].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
 
-    print_step('BASIC FE 15/28')
+    print_step('BASIC FE 15/30')
     train2['chars_per_word'] = train2['num_chars'] / train2['num_words']
     test2['chars_per_word'] = test2['num_chars'] / test2['num_words']
 
-    print_step('BASIC FE 16/28')
-    train2['sentence'] = train2['comment_text'].apply(lambda x: [len(s) for s in re.split(r'[.!?\n]+', str(x))])
-    test2['sentence'] = test2['comment_text'].apply(lambda x: [len(s) for s in re.split(r'[.!?\n]+', str(x))])
+    print_step('BASIC FE 16/30')
+    train2['sentence'] = train2['comment_text'].apply(lambda x: [s for s in re.split(r'[.!?\n]+', str(x))])
+    test2['sentence'] = test2['comment_text'].apply(lambda x: [s for s in re.split(r'[.!?\n]+', str(x))])
 
-    print_step('BASIC FE 17/28')
+    print_step('BASIC FE 17/30')
     train2['num_sentence'] = train2['sentence'].apply(lambda x: len(x))
     test2['num_sentence'] = test2['sentence'].apply(lambda x: len(x))
 
-    print_step('BASIC FE 18/28')
-    train2['sentence_mean'] = train2.sentence.apply(lambda x: np.mean(x))
-    test2['sentence_mean'] = test2.sentence.apply(lambda x: np.mean(x))
+    print_step('BASIC FE 18/30')
+    train2['sentence_mean'] = train2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: np.mean(x))
+    test2['sentence_mean'] = test2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: np.mean(x))
 
-    print_step('BASIC FE 19/28')
-    train2['sentence_max'] = train2.sentence.apply(lambda x: max(x) if len(x) > 0 else 0)
-    test2['sentence_max'] = test2.sentence.apply(lambda x: max(x) if len(x) > 0 else 0)
+    print_step('BASIC FE 19/30')
+    train2['sentence_max'] = train2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: max(x) if len(x) > 0 else 0)
+    test2['sentence_max'] = test2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: max(x) if len(x) > 0 else 0)
 
-    print_step('BASIC FE 20/28')
-    train2['sentence_min'] = train2.sentence.apply(lambda x: min(x) if len(x) > 0 else 0)
-    test2['sentence_min'] = test2.sentence.apply(lambda x: min(x) if len(x) > 0 else 0)
+    print_step('BASIC FE 20/30')
+    train2['sentence_min'] = train2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: min(x) if len(x) > 0 else 0)
+    test2['sentence_min'] = test2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: min(x) if len(x) > 0 else 0)
 
-    print_step('BASIC FE 21/28')
-    train2['sentence_std'] = train2.sentence.apply(lambda x: np.std(x))
-    test2['sentence_std'] = test2.sentence.apply(lambda x: np.std(x))
+    print_step('BASIC FE 21/30')
+    train2['sentence_std'] = train2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: np.std(x))
+    test2['sentence_std'] = test2.sentence.apply(lambda xs: [len(x) for x in xs]).apply(lambda x: np.std(x))
 
-    print_step('BASIC FE 22/28')
+    print_step('BASIC FE 22/30')
     train2['words_per_sentence'] = train2['num_words'] / train2['num_sentence']
     test2['words_per_sentence'] = test2['num_words'] / test2['num_sentence']
+
+    print_step('BASIC FE 23/30')
+    train2['num_repeated_sentences'] = train2['sentence'].apply(lambda x: len(x) - len(set(x)))
+    test2['num_repeated_sentences'] = test2['sentence'].apply(lambda x: len(x) - len(set(x)))
     train2.drop('sentence', inplace=True, axis=1)
     test2.drop('sentence', inplace=True, axis=1)
 
     # From https://www.kaggle.com/ogrellier/lgbm-with-words-and-chars-n-gram
-    print_step('BASIC FE 23/28')
+    print_step('BASIC FE 24/30')
     train2['start_with_columns'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'^\:+', x) else 0)
     test2['start_with_columns'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'^\:+', x) else 0)
 
-    print_step('BASIC FE 24/28')
+    print_step('BASIC FE 25/30')
     train2['has_timestamp'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'\d{2}|:\d{2}', x) else 0)
     test2['has_timestamp'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'\d{2}|:\d{2}', x) else 0)
 
-    print_step('BASIC FE 25/28')
+    print_step('BASIC FE 26/30')
     train2['has_date_long'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'\D\d{2}:\d{2}, \d{1,2} \w+ \d{4}', x) else 0)
     test2['has_date_long'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'\D\d{2}:\d{2}, \d{1,2} \w+ \d{4}', x) else 0)
 
-    print_step('BASIC FE 26/28')
+    print_step('BASIC FE 27/30')
     train2['has_date_short'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'\D\d{1,2} \w+ \d{4}', x) else 0)
     test2['has_date_short'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'\D\d{1,2} \w+ \d{4}', x) else 0)
 
-    print_step('BASIC FE 27/28')
+    print_step('BASIC FE 28/30')
     train2['has_link'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'http[s]{0,1}://\S+', x) else (1 if re.search(r'www\.\S+', x) else 0))
     test2['has_link'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'http[s]{0,1}://\S+', x) else (1 if re.search(r'www\.\S+', x) else 0))
 
-    print_step('BASIC FE 28/28')
+    print_step('BASIC FE 29/30')
     train2['has_email'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', x) else 0)
     test2['has_email'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', x) else 0)
+
+    print_step('BASIC FE 30/30')
+    train2['has_ip_address'] = train2['comment_text'].apply(lambda x: 1 if re.search(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', x) else 0)
+    test2['has_ip_address'] = test2['comment_text'].apply(lambda x: 1 if re.search(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', x) else 0)
 
 
     ## PART OF SPEECH
@@ -200,7 +208,7 @@ def add_features(train, test):
     test2['num_verb'] = test2['pos_space'].apply(lambda ws: len([w for w in ws if 'VB' in w[1]]))
     print_step('POS 17/26')
     train2['verb_per_word'] = train2['num_verb'] / train2['num_words']
-    train2['verb_per_word'] = train2['num_verb'] / train2['num_words']
+    test2['verb_per_word'] = test2['num_verb'] / test2['num_words']
     print_step('POS 18/26')
     train2['num_past_verb'] = train2['pos_space'].apply(lambda ws: len([w for w in ws if 'VBD' in w[1]]))
     test2['num_past_verb'] = test2['pos_space'].apply(lambda ws: len([w for w in ws if 'VBD' in w[1]]))
@@ -212,7 +220,7 @@ def add_features(train, test):
     test2['num_non_third_singular_present_verb'] = test2['pos_space'].apply(lambda ws: len([w for w in ws if 'VBP' in w[1]]))
     print_step('POS 21/26')
     train2['num_modal_then_verb'] = train2['pos_space'].apply(lambda ws: 'MD VB' in ' '.join([w[1] for w in ws]))
-    test2['has_modal_then_verb'] = test2['pos_space'].apply(lambda ws: 'MD VB' in ' '.join([w[1] for w in ws]))
+    test2['num_modal_then_verb'] = test2['pos_space'].apply(lambda ws: 'MD VB' in ' '.join([w[1] for w in ws]))
     print_step('POS 22/26')
     train2['has_personal_pronoun_then_singular_present_verb'] = train2['pos_space'].apply(lambda ws: 'PRP VBP' in ' '.join([w[1] for w in ws]))
     test2['has_personal_pronoun_then_singular_present_verb'] = test2['pos_space'].apply(lambda ws: 'PRP VBP' in ' '.join([w[1] for w in ws]))
@@ -254,7 +262,7 @@ def add_features(train, test):
     test2['num_big_words'] = test2.syllable.apply(lambda xs: len([x for x in xs if x > 2]))
     print_step('SYLLABLE 8/10')
     train2['num_simple_words'] = train2.syllable.apply(lambda xs: len([x for x in xs if x == 1]))
-    train2['num_simple_words'] = train2.syllable.apply(lambda xs: len([x for x in xs if x == 1]))
+    test2['num_simple_words'] = test2.syllable.apply(lambda xs: len([x for x in xs if x == 1]))
     print_step('SYLLABLE 9/10')
     train2['syllable_per_word'] = train2['syllable_sum'] / (train2['num_words'] + 0.0001)
     test2['syllable_per_word'] = test2['syllable_sum'] / (test2['num_words'] + 0.0001)
@@ -302,42 +310,50 @@ def add_features(train, test):
               r'\bworm\b', 'lazy', 'loser', 'noob', r'\bcow', r'\bfool\b', 'ignoramus', 'imbecile',
               'dumb', 'pansy', 'pansies', 'hillbilly', 'trailer trash', 'wetback', 'twat',
               'piece of garbage', 'commie', 'pedo', 'peado', 'douche', 'doushe', 'dousch',
-              'monkey']
+              'monkey', 'boi', 'slime', 'inbred', 'maggot', 'weakling', 'worthless', 'disgrace',
+			  'pompous', 'pathetic', 'nerd']
     n_word = [r'\bnig\b', 'n\*g', 'nigg', 'chink', 'chynk', 'nig nog', 'wigger', 'wigga',
               r'\bjigg']
     mean_identity = ['dyke', 'dike', 'fag', 'gay', 'tranny', 'lesbo', 'queer', 'qweer', 'shemale', r'\bnegro',
-                     r'\bgyp', r'\bgips', 'white trash', 'darkie', 'chinaman', '\bcracker\b', 'eurotrash', '\bsperg', 'aspie',
+                     r'\bgyp', r'\bgips', 'white trash', 'darkie', 'chinaman', r'\bcracker\b', 'eurotrash', r'\bsperg', 'aspie',
                      r'\bjap\b', 'christ killer', r'\bhomo\b', 'redneck', 'jew-hat', 'jew hat',
                      r'\bturk\b', 'hermaphrodite', r'\bretard\b', 'subhuman']
     identity = ['lesbian', 'bisexual', r'\btrans\b', 'transex', 'lesbian', r'jew\b', 'jewish', 'black', 'arab',
                 'muslim', 'asian', 'islam', 'jihad', 'mexican', 'african', 'irish', 'homosexual',
                 'frenchman', 'niger', 'transvest', 'afghan', 'armenian', 'speak english', 'semit',
-                'christian', 'chinese', 'manchu']
+                'christian', 'chinese', 'manchu', 'white', 'indian', 'women', r'\bnation\b']
     severe_toxic = ['c0ck', 'cock', 'cawk', r'clit\b', 'cunt', 'c\*nt', r'\bcum\b', 'fuc', 'fuk']
     toxic = ['crap', 'blowjob', 'blows me', 'blow me', 'go blow', 'butthole', 'buttwipe', 'dick',
-            'dildo', r'\bf u\b', 'fart', 'gook', 'damn', r'goddam\b', 'f\*ck', 'f\*\*', 'di\*k',
+            'dildo', r'\bf u\b', 'fart', 'gook', 'damn', r'goddam\b', r'f.ck', 'f\*\*', 'di\*k',
             'a$$', 'masturb', 'shit', 'sh1t', 'sh!t', r'\btit[^-][^f]', 'turd', 'arse', 'balls',
             'suck', 'fcuk', 'mofo', 'nazi', 'phuck', 'boob', 'b00b', 'wank', '@ss', '@$$',
             'breast', 'cabron', 'feces', 'poop', 'porn', 'p0rn', 'screw', r'\bf you\b',
             r'\bstfu\b', 'a-hole', 'wtf', 'rubbish', r'\banal\b', r'\bpeto', 'prostitut',
             'shut up', 'scum', 'thug', 'filth', 'rapist', 'bagger', 'communist', 'racist', 'hate',
-            'sodomize', 'mother f ']
-    obscene = ['jack off', 'jizz', 'penis', 'semen', 'vagina', 'ejaculate', 'test2icle', 'vajay', 'piss', 'pecker']
+            'sodomize', 'mother f ', 'lowly', 'cancer', 'spews', r'\bhag\b', 'tr0ll', r'\bha\b',
+			'haha', 'cry', 'lover', 'loving', 'sick', 'smell', 'freak', r'basement\b', 'anus']
+    obscene = ['jack off', 'jizz', 'penis', 'semen', 'vagina', 'ejaculate', 'testicle', 'vajay',
+			   'piss', 'pecker', 'urinate']
     wiki = ['wiki', 'jimbo', 'talk', 'page', 'edit', 'signed', 'block', 'edit', 'contribute', 'bot',
             'image:', 'file:', 'deletion', 'user:', '==', r'\baccount\b', 'sock puppet', 'sockpuppet',
-            'barnstar']
+            'barnstar', 'tutorial', 'source']
     threat = ['bludgeon', 'beat', 'to death', r'\bshoot', 'die', r'\bshot', 'dead', 'kill', 'punch',
               'burn', 'blood', 'destroy', r'\bcut\b', 'rape', 'stab', r'knock you\b', 'kick', 'hunt you', 'abuse',
               r'happen to you\b', 'better watch', 'suicide', 'execute', 'blow up', r'\bso watch',
               'been warned', 'where you live', 'attack', 'warning', 'track you down', 'hope u die',
-              'hope you die', 'skin you']
-    objects = ['you', 'my', '\bur\b', 'urself', '\bu r\b', r'\bmum', r'mom\b', r'\bmother\b', r'\bis a\b', r'\bis an\b', r'\bgo\b']
+              'hope you die', 'skin you', 'infadel', r'\bchoke\b', r'\broast\b', 'succumb', 'corpse', 'agony',
+			  'stomp', r'\bsever\b', 'painful', 'petrol', 'snitch', 'shotgun', 'kidnap', 'smack', 'scream',
+			  'guts', 'bleed', r'\bshove\b', r'\bsmash\b', 'stalker', 'stalking', r'\bhang\b', r'\bdrown\b',
+			  r'\bslit\b', 'castrate', 'gonna', r'\brot\b']
+    objects = ['you', 'my', r'\bur\b', 'urself', r'\bu r\b', r'\bmum', r'mom\b', r'\bmother\b', r'\bis a\b', r'\bis an\b',
+			  r'\bgo\b', 'mutha']
+    self = [r'\bme\b', r'\bi\b', r'\bmy\b', 'myself']
     qualifiers = ['offensive', 'derogatory']
     polite = ['thank', 'help', 'please', 'welcome']
 
     count = 0
     total = len(insult) + len(n_word) + len(mean_identity) + len(identity) + len(severe_toxic) + len(toxic) + \
-            len(obscene) + len(wiki) + len(threat) + len(objects) + len(qualifiers) + len(polite) + 14
+            len(obscene) + len(wiki) + len(threat) + len(objects) + len(qualifiers) + len(self) + len(polite) + 15
 
     for i in insult:
         count += 1
@@ -429,6 +445,15 @@ def add_features(train, test):
     train2['has_cat_objects'] = train2[['has_' + str(i) for i in objects]].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
     test2['has_cat_objects'] = test2[['has_' + str(i) for i in objects]].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
 
+    for i in self:
+        count += 1
+        print_step('BAD WORDS ' + str(count) + '/' + str(total) + ': ' + str(i))
+        train2['has_' + str(i)] = train2.comment_text.apply(lambda t: 1 if re.search(i, t.lower()) else 0)
+        test2['has_' + str(i)] = test2.comment_text.apply(lambda t: 1 if re.search(i, t.lower()) else 0)
+    count += 1; print_step('BAD WORDS ' + str(count) + '/' + str(total))
+    train2['has_cat_self'] = train2[['has_' + str(i) for i in self]].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
+    test2['has_cat_self'] = test2[['has_' + str(i) for i in self]].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
+
     for i in qualifiers:
         count += 1
         print_step('BAD WORDS ' + str(count) + '/' + str(total) + ': ' + str(i))
@@ -448,8 +473,8 @@ def add_features(train, test):
     test2['has_cat_polite'] = test2[['has_' + str(i) for i in polite]].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
 
     count += 1; print_step('BAD WORDS ' + str(count) + '/' + str(total))
-    train2['num_bad_words'] = train2[[c for c in train2.columns if 'has' in c and 'has_cat' not in c]].sum(axis=1) - train2[['has_' + str(i) for i in qualifiers] + ['has_' + str(i) for i in wiki] + ['has_' + str(i) for i in objects] + ['has_' + str(i) for i in polite]].sum(axis=1)
-    test2['num_bad_words'] = test2[[c for c in test2.columns if 'has' in c and 'has_cat' not in c]].sum(axis=1) - test2[['has_' + str(i) for i in qualifiers] + ['has_' + str(i) for i in wiki] + ['has_' + str(i) for i in objects]].sum(axis=1)
+    train2['num_bad_words'] = train2[[c for c in train2.columns if 'has' in c and 'has_cat' not in c]].sum(axis=1) - train2[['has_' + str(i) for i in qualifiers] + ['has_' + str(i) for i in wiki] + ['has_' + str(i) for i in objects] + ['has_' + str(i) for i in polite] + ['has_' + str(i) for i in self]].sum(axis=1)
+    test2['num_bad_words'] = test2[[c for c in test2.columns if 'has' in c and 'has_cat' not in c]].sum(axis=1) - test2[['has_' + str(i) for i in qualifiers] + ['has_' + str(i) for i in wiki] + ['has_' + str(i) for i in objects] + ['has_' + str(i) for i in polite] + ['has_' + str(i) for i in self]].sum(axis=1)
 
     count += 1; print_step('BAD WORDS ' + str(count) + '/' + str(total))
     train2['bad_words_per_word'] = train2['num_bad_words'] / train2['num_words']
